26e0855887038a1a6f183daec63c324349e02e1b
aishasheriff <60453604+aishasheriff@users.noreply.github.com>
2020-03-10 11:07:25 -0400

    Update turning__.ino

--- turning__.ino
+++ turning__.ino
@@ -4,12 +4,13 @@
 // encoder inputs 
 //int outA = 10 , outB = 11; // connect encoder inputs to regular digital ports 
 // 
-int enA = 2;// enableA and B have to into PWM I/O so that we can regulate speed 
-int enB = 3; 
-int in1 = 38; // in1 and in2, 3, 4 are used to control the rotational direction of the wheels
-int in2 = 41; // pin 38 and 41 for motor1 
-int in3 = 42; // pin 42 and 45 for motor 2 
-int in4 = 45; 
+int en3 = 2;// enableA and B have to into PWM I/O so that we can regulate speed 
+int en6 = 3; 
+
+int en1 = 38; // in1 and in2, 3, 4 are used to control the rotational direction of the wheels
+int en2 = 41; // pin 38 and 41 for motor1 
+int en4 = 42; // pin 42 and 45 for motor 2 
+int en5 = 45; 
 
 float error;
 
@@ -26,12 +27,12 @@ void setup(){
   
   pinMode(outA,INPUT);
   pinMode(outB,INPUT);
-  pinMode(in1,OUTPUT);
-  pinMode(in2,OUTPUT);
-  pinMode(in3,OUTPUT);
-  pinMode(in4,OUTPUT);
-  pinMode(enA,OUTPUT);
-  pinMode(enB,OUTPUT);
+  pinMode(en1,OUTPUT);
+  pinMode(en2,OUTPUT);
+  pinMode(en3,OUTPUT);
+  pinMode(en5,OUTPUT);
+  pinMode(en3,OUTPUT);
+  pinMode(en6,OUTPUT);
   
 }
 //Function for moving forward 
@@ -53,34 +54,34 @@ void loop(){
   
        if(distancefront()==ESTOP){
       
-            if(distanceright()>(tollerence+distanceleft())){ //turn right 
+            if(distanceright()>(Error+distanceleft())){ //turn right 
 
                      while(distanceright()!=distanceleft())  //in a loop until left and right sensors are equal, therfore showing  theres no wall longer and its in go straight orientation
                        //right motor 
-                      analogWrite(enA, 150); // Send PWM signal to motor A
-                      digitalWrite(in1,HIGH);//setting left motorB to move forward
-                      digitalWrite(in2,LOW); 
-
+                     
+                      digitalWrite(en1,HIGH);//setting left motorB to move forward
+                      digitalWrite(en2,LOW); 
+                       analogWrite(en3, 150); // Send PWM signal to motor A
                       //leftmotor 
-                      digitalWrite(in3,LOW); 
-                      digitalWrite(in4,HIGH);// setting left motor to move in opposite diesction
-                      analogWrite(enB, 50); // Speed of motor B very little, might end up making it 0
+                      digitalWrite(en4,LOW); 
+                      digitalWrite(en5,HIGH);// setting left motor to move in opposite diesction
+                      analogWrite(en6, 50); // Speed of motor B very little, might end up making it 0
                       
                       delay(1000)
                         }
 
-               else if(distanceleft()>(tollerence+distanceright()){ // turn Left
+               else if(distanceleft()>(Error+distanceright()){ // turn Left
                      //right motor 
                       while(distanceleft()!=distanceright()) // in a loop until left and right sensors are equal, therefore showing theres no wall longer and its in go straight orientation
-                      digitalWrite(in1,LOW);
-                      digitalWrite(in2,HIGH);
-                      analogWrite(enA, 50);
+                      digitalWrite(en1,LOW);
+                      digitalWrite(en2,HIGH);
+                      analogWrite(en3, 50);
 
                       //left motor 
                       
-                      digitalWrite(in3,HIGH);
-                      digitalWrite(in4,LOW);
-                      analogWrite(enB, 150);
+                      digitalWrite(en4,HIGH);
+                      digitalWrite(en5,LOW);
+                      analogWrite(en6, 150);
                       delay(1000)
                        }
                else{
@@ -90,13 +91,13 @@ void loop(){
                             //making both motors go back ward until Estop is false  
                             while(distancefront()<(ESTOP+distanceofoneblock)) { //runs until it leaves dead (assuming we make deadends one block deep)
                               
-                            digitalWrite(in1,LOW);
-                            digitalWrite(in2,HIGH);
-                            analogWrite(enA, 150);
+                            digitalWrite(en1,LOW);
+                            digitalWrite(en2,HIGH);
+                            analogWrite(en3, 150);
                             
-                            digitalWrite(in3,LOW);
-                            digitalWrite(in4,HIGH);
-                            analogWrite(enB, 150);
+                            digitalWrite(en4,LOW);
+                            digitalWrite(en5,HIGH);
+                            analogWrite(en6, 150);
                              }
                   
                     }
